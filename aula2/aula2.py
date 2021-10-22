7aa1237f19ee5268ae18fa19042cd4fc2bd68c01
JansenDF <jansen.lima.df@gmail.com>
2021-10-22 12:43:07 -0300

    comando print

---
+++ aula2/aula2.py
@@ -0,0 +1,10 @@
+
+print(12345)
+print('Jansen', 'Lima', 'Outra coisa')
+print('Jansen', 'Lima', sep='-')
+print('Jansen', end='')
+print('Lima')
+
+#Imprimir CPF usando separadores específicos
+print('824', '176', '070', sep='.', end='-')
+print('18')
