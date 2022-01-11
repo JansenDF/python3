f9baec5a675c684ac0dc10a9807a8ca73cc0313a
Jansen <jansen.lima.df@gmail.com>
2022-01-11 13:07:50 -0300

    Adicionado aula4, sobre tipos de dados

---
+++ aula4/aula4.py
@@ -0,0 +1,16 @@
+"""
+Tipos de Dados
+str - string
+int - inteiro
+float - ponto flutuante
+bool - booleano
+
+"""
+#nome
+print('Jansen', type('Jansen'))
+#idade
+print(34, type(34))
+#altura
+print(1.75, type(1.75))
+#maior de idade
+print(34 > 18, type(34>18))
