1a7efbb77aa5387ad9a8c553561e30d1ff2c2c98
Jansen <jansen.lima.df@gmail.com>
2022-01-11 13:54:33 -0300

    Adicionado aula 6 sobre variaveis e seu uso

---
+++ aula6/aula6.py
@@ -0,0 +1,16 @@
+"""
+Variaveis - Iniciar com letra, pode conter numeros, separar _, letras minúsculas
+"""
+nome = 'Jansen'
+idade = 34
+altura = 1.75
+e_maior = idade > 18
+peso = 80
+imc = peso / (altura * altura)
+
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('É maior? ', e_maior)
+
+print(nome, 'tem', idade, 'anos de idade e seu IMC é: ', imc)
