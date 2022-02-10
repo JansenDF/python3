cf1afee7ef22c9cef15b46ed925520e1c481aaf0
Jansen Soares de Lima <jansen.lima.df@gmail.com>
2022-02-10 13:03:10 -0300

    Usando a função LEN - Quantidade de caracteres.

---
+++ aula9/len.py
@@ -0,0 +1,12 @@
+usuario = input('Nome do usuário: ')
+
+qtde_caractere = len(usuario)
+
+print(f'{usuario} possui {qtde_caractere} caracteres.')
+
+if qtde_caractere < 5:
+    print('Usuário possui menos de 5 caractere!')
+elif (qtde_caractere > 5) and (qtde_caractere < 10):
+    print('Usuário possui mais de 5 e menos de 10 caracteres!')
+else:
+    print('Usuário poaaui mais de 10 caracteres!')
